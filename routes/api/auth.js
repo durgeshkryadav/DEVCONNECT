5585bb38294999082e5903abc360422a004380e1
Durgesh Kumar Yadav <57389386+DGSH9@users.noreply.github.com>
2022-01-01 02:29:35 +0530

    User Authentication Login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,89 +1,81 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../../middleware/auth');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const auth = require("../../middleware/auth");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
 
-router.get('/', auth, async (req, res) => {
-    try{
-        const user = await User.findById(req.user.id).select('-password');
-        res.json(user);
-
-    }catch(err){
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
-
-
-
 
 // @route   POST api/auth
 // @desc    Authenticate User and get token
 // @access  Public
 router.post(
-    '/',
-    [
-    check('email', 'please include a valid email').isEmail(),
-    check('password', 'Password is required').exists()
-  
+  "/",
+  [
+    check("email", "please include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
   ],
-    
-    
-    async(req, res) => {
-      // console.log(req.body);
-      const errors = validationResult(req);
-      if(!errors.isEmpty()){
-          return res.status(400).json({ errors: errors.array() });
-      }
-  
-      const { email, password } = req.body;
-      
-      try{
-            let user = await User.findOne({ email });
-            if(!user)
-            {
-              return res.status(400).json({errors: [{ msg: 'Invalid Credentials' }] });
-         }
-            
-         
-         const isMatch = await bcrypt.compare(password, user.password);
 
-
-           
-            // res.send('User registred');
-            const payload = {
-              user:{
-                id:user.id
-              }
-            }
-  
-            jwt.sign( 
-              payload, 
-              config.get('jwtSecret'),
-              { expiresIn: 7200000},
-              (err,token)=>{
-                if(err) throw err;
-                res.json({token});
-              });
-  
-  
-  
-  
-      }
-      catch(err){
-              console.error(err.message);
-              res.status(500).send('Server Error Dubara Try Kariye Please');
-      }    
+  async (req, res) => {
+    // console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  );
 
-module.exports = router; 
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+      // res.send('User registred');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 7200000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
