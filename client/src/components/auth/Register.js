454da50b0539d6cff0e24139161c37d5fbf34f7c
Durgesh Kumar Yadav <57389386+DGSH9@users.noreply.github.com>
2022-01-03 04:01:33 +0530

    getting token id of user through browser

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
+
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -11,20 +13,43 @@ const Register = () => {
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
             console.log('Password dont match');
         } else {
-            console.log(formData);
+            const newUser = {
+                name,
+                email,
+                password
+            };
+            try {
+                const config = {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                };
+
+                const body = JSON.stringify(newUser);
+
+                const res = await axios.post('/api/users', body, config);
+
+                console.log(res.data);
+
+            } catch (err) {
+                console.error(err.response.data);
+            }
         }
-    }
+    };
+
+
+
     return <Fragment>
         <h1 className="large text-primary">Sign Up</h1>
         <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+
+
         <form className="form" onSubmit={e => onSubmit(e)}>
-
-
             {/* for name */}
             <div className="form-group">
                 <input type="text" placeholder="Name" name="name"
