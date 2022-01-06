ae8ce05e13e3249a59d9a786469d496f8c280d5d
Durgesh Kumar Yadav <57389386+DGSH9@users.noreply.github.com>
2022-01-06 23:21:14 +0530

    7.4 Alert Component & Action Call(4:00 min)

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,9 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
 
-const Register = () => {
+const Register = (props) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -15,7 +17,7 @@ const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
-            console.log('Password dont match');
+            props.setAlert('Password dont match', 'danger');
         } else {
             console.log('SUCCESS');
         }
@@ -81,4 +83,4 @@ const Register = () => {
 
     </Fragment>
 };
-export default Register;
+export default connect(null, { setAlert })(Register);
---
+++ tempCodeRunnerFile.javascript
@@ -0,0 +1,4 @@
+arr1 = [1,2,3,4,5]
+
+arr1.shift(arr1[0])
+console.log(arr1);
