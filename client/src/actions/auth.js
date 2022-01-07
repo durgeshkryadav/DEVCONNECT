ff1ffd7bc02b13c5c433af88fc98f4befb8b1b59
Durgesh Kumar Yadav <57389386+DGSH9@users.noreply.github.com>
2022-01-07 15:59:20 +0530

    8.4 Logout & Navbar Links

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from '../actions/types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from '../actions/types';
 import setAuthToken from '../utils/setAuthToken';
 
 
@@ -100,4 +100,15 @@ export const login = (email, password) => async dispatch => {
             type: LOGIN_FAIL
         });
     }
+}
+
+
+
+
+// LOGOUT / CLear Profile
+
+export const logout = () => dispatch => {
+    dispatch({
+        type: LOGOUT
+    });
 }
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -9,3 +9,6 @@ export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 
+export const LOGOUT = 'LOGOUT';
+
+
