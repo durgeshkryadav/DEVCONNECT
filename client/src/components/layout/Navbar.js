6d782c8260904d11d039132cdb33aea1f06da689
Durgesh Kumar Yadav <57389386+DGSH9@users.noreply.github.com>
2022-01-08 21:53:49 +0530

    9.3 Going to add Spinner.js

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,6 +7,12 @@ import { logout } from '../../actions/auth';
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     const authLinks = (
         <ul>
+            <li>
+                <Link to="/dashboard">
+                    <i className="fas fa-user"></i>{' '}
+                    <span className="hide-sm">Dashboard</span></Link>
+            </li>
+
             <li><a onClick={logout} href="#!">
                 <i className="fas fa-sign-out-alt"></i>{' '}
                 <span className="hide-sm">Logout</span></a>
@@ -17,8 +23,8 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     const guestLinks = (
         <ul>
             <li><a href="#!">Developers</a></li>
-            <li><Link to="register">Register</Link></li>
-            <li><Link to="login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
+            <li><Link to="/login">Login</Link></li>
         </ul>
     );
 
